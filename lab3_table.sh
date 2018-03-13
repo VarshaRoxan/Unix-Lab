be5865e779fb7691c1b4e34da0b5d6ca2a946443
VarshaRoxan <35593941+VarshaRoxan@users.noreply.github.com>
2018-03-13 14:47:50 +0530

    Add files via upload

---
+++ lab3_table.sh
@@ -0,0 +1,17 @@
+USN	NAMES		CGPA
+1	varsha		8
+2	vaibhava	7
+3	venkatesh	4
+4	tejas		9
+5	srinivas	6
+6	sushanth	7
+7	sowbhagya	3
+8	swathi 		6
+9	sneha		9
+10	srikanth	8
+11	hitesh		6
+12	kishore		7
+13	prajwal		5
+14	pranav		9
+15	shobi		6
+
